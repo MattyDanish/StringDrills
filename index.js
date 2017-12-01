50b0fdc23d85930d818ead1ace652ad43e8a4606
Matthew Delisle <matthewdelisle@MatthewislesMBP.home>
2017-12-01 13:52:23 -0500

    StringDrills

---
+++ index.js
@@ -0,0 +1,19 @@
+//string drill 1//
+
+function wisePerson(wiseType, whatToSay) {
+  return 'A wise ' + wiseType + ' once said: "' + whatToSay + '".';
+}
+
+
+//string drill 2//
+
+function shouter(whatToShout) {
+  return whatToShout.toUpperCase() + '!!!';
+}
+
+
+//string drill 3//
+
+function textNormalizer(text){
+  var res = text.toLowerCase().trim();
+}
